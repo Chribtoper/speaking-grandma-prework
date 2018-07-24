72ca7511edcb7e0a5f6f9023a5c2edeb98011cf3
Chribtoper <xcstolfax@gmail.com>
2018-07-24 21:36:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(speak)
   else
     "HUH?! SPEAK UP, SONNY!"
   end
-   elsif speak == "I LOVE YOU GRANDMA!"
+   if speak == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
 end
